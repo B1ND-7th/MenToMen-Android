b7291a3d11d42dcb31fde8c69392e9d17b95f7bd
Seokgyu <sk71521124@gmail.com>
2022-08-30 07:40:42 +0900

    Add: Main Ui

--- app/src/main/java/kr/hs/b1nd/intern/mentomen/view/fragment/HomeFragment.kt
+++ app/src/main/java/kr/hs/b1nd/intern/mentomen/view/fragment/HomeFragment.kt
@@ -5,17 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModelProvider
 import kr.hs.b1nd.intern.mentomen.R
+import kr.hs.b1nd.intern.mentomen.databinding.FragmentHomeBinding
+import kr.hs.b1nd.intern.mentomen.network.model.ImageItem
+import kr.hs.b1nd.intern.mentomen.view.adapter.HomeAdapter
+import kr.hs.b1nd.intern.mentomen.viewmodel.HomeViewModel
 
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var homeViewModel: HomeViewModel
+
+    private lateinit var homeAdapter: HomeAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_home,
+            container,
+            false
+        )
+        performViewModel()
+        observeViewModel()
+
+        return binding.root
     }
 
+    private fun observeViewModel() {
+        with(homeViewModel) {
+            itemList.observe(viewLifecycleOwner) {
+                initHomeAdapter(it)
+            }
+        }
+    }
+
+    private fun initHomeAdapter(items: ArrayList<ImageItem>) {
+        homeAdapter = HomeAdapter(items)
+        binding.rvHome.adapter = homeAdapter
+        homeAdapter.notifyDataSetChanged()
+    }
+
+    private fun performViewModel() {
+        homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+        binding.vm = homeViewModel
+        binding.lifecycleOwner = this
+        binding.executePendingBindings()
+    }
 }

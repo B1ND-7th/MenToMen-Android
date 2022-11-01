907650500b25fc26f3b12e361954bbd386c7d882
Seokgyu <sk71521124@gmail.com>
2022-11-01 16:45:33 +0900

    Fix: Version

--- app/src/main/java/kr/hs/b1nd/intern/mentomen/util/Constants.kt
+++ app/src/main/java/kr/hs/b1nd/intern/mentomen/util/Constants.kt
@@ -1,7 +1,7 @@
 package kr.hs.b1nd.intern.mentomen.util
 
 object Constants {
-    const val BASE_URL = "http://mentomen.b1nd.com/api/"
-//    const val BASE_URL = "http://10.80.161.227:8080/api/"
+//    const val BASE_URL = "http://mentomen.b1nd.com/api/"
+    const val BASE_URL = "http://101.101.209.184:7030/api/"
     const val LOGIN_URL = "http://dauth.b1nd.com/api/auth/"
 }
